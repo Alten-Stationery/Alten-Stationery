73e38037dc60ac695fb9b02371c2f4e374fff996
FanelliAlessandro <alessandro.fanelli@alten.it>
2024-06-07 12:41:31 +0200

    Create Refills Repository

--- Alten Stationery/obj/Debug/net8.0-windows/Alten Stationery.AssemblyInfo.cs
+++ Alten Stationery/obj/Debug/net8.0-windows/Alten Stationery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alten Stationery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e8480e25b7841c7efe180eda5e777844c60788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde7551949d120de17fca3fb35041f715356847d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alten Stationery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alten Stationery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
