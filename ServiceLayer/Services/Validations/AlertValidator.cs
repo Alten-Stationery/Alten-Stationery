8c63bb10751da242ac7740a1bb5ac059d7e4856f
Vittorio GIORDANO <vittorio.giordano@alten.it>
2024-06-11 10:53:37 +0200

    Service Alert with Validation

---
+++ ServiceLayer/Services/Validations/AlertValidator.cs
@@ -0,0 +1,33 @@
+﻿using DBLayer.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services.Validations
+{
+    public class AlertValidator : AbstractValidator<Alert>
+    {
+
+        public bool CheckDate(DateTime date)
+        {
+            if (date > DateTime.Today)
+                return true;
+            return false;
+        }
+
+        public AlertValidator()
+        {
+
+            RuleFor(x => x.AlertDate).NotEmpty().Must(CheckDate);
+
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("A valid Email is required!");
+
+            RuleFor(x => x.EmailBody).NotNull().NotEmpty();
+
+        }
+
+    }
+}
