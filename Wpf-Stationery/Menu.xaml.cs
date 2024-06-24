d851ad4fe18c4b9b3206a9482b509e8336bbb47b
Vittorio GIORDANO <vittorio.giordano@alten.it>
2024-06-24 12:47:19 +0200

    Template base Dashboard

---
+++ Wpf-Stationery/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Stationery
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : NavigationWindow
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Link_Dashboard(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
