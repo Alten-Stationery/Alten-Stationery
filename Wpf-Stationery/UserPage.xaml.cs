ee1cc28028c424d180d6fc30c8327475b4eceab4
FanelliAlessandro <alessandro.fanelli@alten.it>
2024-06-26 09:41:35 +0200

    Create BaseLayout

--- Wpf-Stationery/UserPage.xaml.cs
+++ Wpf-Stationery/UserPage.xaml.cs
@@ -29,5 +29,25 @@ namespace Wpf_Stationery
         {
 
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
---
+++ Wpf-Stationery/ViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+using Alten_Stationery;
+
+namespace Wpf_Stationery
+{
+    public class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        // Close App
+        public void CloseApp(object obj)
+        {
+            MainWindow win = obj as MainWindow;
+            win.Close();
+        }
+
+
+        // Close App Command
+        private ICommand _closeCommand;
+
+        public ICommand CloseAppCommand
+        {
+            get
+            {
+                if (_closeCommand == null)
+                {
+                    _closeCommand = new RelayCommand(p => CloseApp(p));
+                }
+                return _closeCommand;
+            }
+        }
+
+        // Maximize App
+        public void MaxApp(object obj)
+        {
+            MainWindow win = obj as MainWindow;
+
+            if (win.WindowState == WindowState.Normal)
+            {
+                win.WindowState = WindowState.Maximized;
+            }
+            else if (win.WindowState == WindowState.Maximized)
+            {
+                win.WindowState = WindowState.Normal;
+            }
+        }
+
+        // Maximize App Command
+        private ICommand _maxCommand;
+        public ICommand MaxAppCommand
+        {
+            get
+            {
+                if (_maxCommand == null)
+                {
+                    _maxCommand = new RelayCommand(p => MaxApp(p));
+                }
+                return _maxCommand;
+            }
+        }
+
+        public void MainViewModel()
+        {
+            // Set SideMenu Visibility 
+            IsPanelVisible = false;
+        }
+
+        // Close Menu
+        public void CloseMenu()
+        {
+            IsPanelVisible = false;
+        }
+
+        // Show Menu
+        public void ShowMenu()
+        {
+            IsPanelVisible = true;
+        }
+
+        // Show Menu Command
+        private ICommand _showMenuCommand;
+
+        public ICommand ShowMenuCommand
+        {
+            get
+            {
+                if (_showMenuCommand == null)
+                {
+                    _showMenuCommand = new RelayCommand(p => ShowMenu());
+                }
+                return _showMenuCommand;
+            }
+        }
+
+
+        // Close Menu Command
+        private ICommand _closeMenuCommand;
+
+        public ICommand CloseMenuCommand
+        {
+            get
+            {
+                if (_closeMenuCommand == null)
+                {
+                    _closeMenuCommand = new RelayCommand(p => CloseMenu());
+                }
+                return _closeMenuCommand;
+            }
+        }
+
+        private bool _isPanelVisible;
+        public bool IsPanelVisible
+        {
+            get
+            {
+                return _isPanelVisible;
+            }
+            set
+            {
+                _isPanelVisible = value;
+                OnPropertyChanged("IsPanelVisible");
+            }
+        }
+    }
+}
